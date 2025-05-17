0518bfc813f28139b975d6b28ed451e380552d5c
Yryy30 <mhmd.arr30@gmail.com>
2025-05-17 17:51:51 +0700

    update Kriteria view to enhance table structure and improve data representation

--- resources/views/livewire/smart/kriteria.blade.php
+++ resources/views/livewire/smart/kriteria.blade.php
@@ -112,11 +112,40 @@
                 </thead>
                 <tbody>
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> > Diberikan </td>
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> Eksklusif </td>
                         <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> 5 </td>
                     </tr>
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> > Tidak diberikan </td>
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> Tidak Eksklusif </td>
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> 3 </td>
+                    </tr>
+                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> Tidak Diberikan </td>
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> 1 </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="overflow-x-auto mt-5">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th colspan="2" class="px-6 py-3 text-center text-md font-semibold bg-gray-100 dark:bg-gray-800 dark:text-white">
+                        MP-ASI
+                    </th>
+                </tr>
+                <tr>
+                    <th scope="col" class="px-6 py-3">Kondisi</th>
+                    <th scope="col" class="px-6 py-3">Nilai</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> Diberikan </td>
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> 5 </td>
+                    </tr>
+                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> Tidak Diberikan </td>
                         <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> 1 </td>
                     </tr>
                 </tbody>
@@ -141,7 +170,7 @@
                         <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> 5 </td>
                     </tr>
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> > Sebagian layak </td>
+                        <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> Sebagian layak </td>
                         <td class="px-6 py-2 text-gray-600 dark:text-gray-300"> 3 </td>
                     </tr>
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
