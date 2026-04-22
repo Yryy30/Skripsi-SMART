8df27dad957dcc4ab009b5deb4052c90a1fe8812
Yryy30 <mhmd.arr30@gmail.com>
2026-04-22 20:50:54 +0700

    refactor BalitaSeeder to generate random data for seeding

--- database/seeders/BalitaSeeder.php
+++ database/seeders/BalitaSeeder.php
@@ -13,29 +13,24 @@ class BalitaSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
-            [1,'ALVARO KENZIE DEVANNA','L','2024-10-10','RT 05','ANA','2025-09-10 19:09:22','2025-09-10 19:12:24'],
-            [2,'AZZURA N. R.','P','2023-08-15','RT 05','ADLA','2025-09-10 19:10:46','2025-09-10 19:12:41'],
-            [3,'DELIA AMELIA P.','P','2021-06-20','RT 05','RIZKI','2025-09-10 19:12:08','2025-09-10 19:12:08'],
-            [4,'ERDANA','L','2023-02-05','RT 04','ANGGI','2025-09-10 19:13:46','2025-09-10 19:13:46'],
-            [5,'M. HAFIDZ ALFATIH','L','2024-11-06','RT 04','LILIS','2025-09-10 19:14:41','2025-09-10 19:14:41'],
-            [6,'M. RASYID FADHILAH','L','2022-04-14','RT 05','DEWI','2025-09-10 19:15:50','2025-09-10 19:15:50'],
-            [7,'NADIRA ZIA S.','P','2022-01-31','RT 02','RIA H.','2025-09-10 19:16:56','2025-09-10 19:16:56'],
-            [8,'RACHEL NADIA A.','P','2021-03-22','RT 04','EKO','2025-09-10 19:18:10','2025-09-10 19:18:10'],
-            [9,'ZAURA MAZAYA X.','P','2022-11-10','RT 02','TIARA','2025-09-10 19:18:58','2025-09-10 19:18:58'],
-            [10,'M. KAILANDRA A.','L','2025-02-01','RT 02','ISNA NIA','2025-09-10 19:19:54','2025-09-10 19:19:54'],
-        ];
+        for ($i = 0; $i < 20; $i++) {
+            $rand = rand(1, 100);
+            if ($rand <= 70) {
+                $tgl_lahir = fake()->dateTimeBetween('-3 years', '-1 years');
+            } elseif ($rand <= 85) {
+                $tanggalLahir = fake()->dateTimeBetween('-1 years', 'now');
+            } else {
+                $tanggalLahir = fake()->dateTimeBetween('-5 years', '-3 years');
+            }
 
-        foreach ($data as $item) {
             DB::table('tbl_balita')->insert([
-                'balita_id'             => $item[0],
-                'nama_balita'           => $item[1],
-                'jenis_kelamin'         => $item[2],
-                'tanggal_lahir'         => $item[3],
-                'alamat'                => $item[4],
-                'nama_orangtua'         => $item[5],
-                'created_at'            => $item[6],
-                'updated_at'            => $item[7],
+                'nama_balita' => fake()->firstName(),
+                'jenis_kelamin' => fake()->randomElement(['L', 'P']),
+                'tanggal_lahir' => $tanggalLahir->format('Y-m-d'),
+                'alamat' => fake()->address(),
+                'nama_orangtua' => fake()->name(),
+                'created_at' => now(),
+                'updated_at' => now(),
             ]);
         }
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             KriteriaSeeder::class,
             //BalitaSeeder::class,
-            //AlternatifSeeder::class,
             LhfaSeeder::class,
             WfaSeeder::class,
         ]);
