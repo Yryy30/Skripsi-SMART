2053181408af1609a49f2f9f58bf49999832b833
Yryy <94096852+Yryy30@users.noreply.github.com>
2026-04-22 21:08:33 +0700

    Refactor faker usage in BalitaSeeder

--- database/seeders/BalitaSeeder.php
+++ database/seeders/BalitaSeeder.php
@@ -13,22 +13,24 @@ class BalitaSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = \fake();
+        
         for ($i = 0; $i < 20; $i++) {
             $rand = rand(1, 100);
             if ($rand <= 70) {
-                $tanggalLahir = fake()->dateTimeBetween('-3 years', '-1 years');
+                $tanggalLahir = $faker->dateTimeBetween('-3 years', '-1 years');
             } elseif ($rand <= 85) {
-                $tanggalLahir = fake()->dateTimeBetween('-1 years', 'now');
+                $tanggalLahir = $faker->dateTimeBetween('-1 years', 'now');
             } else {
-                $tanggalLahir = fake()->dateTimeBetween('-5 years', '-3 years');
+                $tanggalLahir = $faker->dateTimeBetween('-5 years', '-3 years');
             }
 
             DB::table('tbl_balita')->insert([
-                'nama_balita' => fake()->firstName(),
-                'jenis_kelamin' => fake()->randomElement(['L', 'P']),
+                'nama_balita' => $faker->firstName(),
+                'jenis_kelamin' => $faker->randomElement(['L', 'P']),
                 'tanggal_lahir' => $tanggalLahir->format('Y-m-d'),
-                'alamat' => fake()->address(),
-                'nama_orangtua' => fake()->name(),
+                'alamat' => $faker->address(),
+                'nama_orangtua' => $faker->name(),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
