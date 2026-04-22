1abe01711167caa8c4c9a0d35439c3bc951ee3e0
Yryy30 <mhmd.arr30@gmail.com>
2026-04-22 21:45:51 +0700

    Add HasFactory trait to Balita model

--- app/Models/Balita.php
+++ app/Models/Balita.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Balita extends Model
 {
+    use HasFactory;
+
     protected $table = 'tbl_balita';
     protected $primaryKey = 'balita_id';
     public $incrementing = true;
